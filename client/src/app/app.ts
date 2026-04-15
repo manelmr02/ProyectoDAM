29105f4652064bdb2d0ad9a182be3c5dad5d1c81
José Manuel Morales Rivera <manelmoralesrivera@gmail.com>
2026-04-15 16:30:19 +0200

    Update Logo & Name
    Update con el logo y el nombre del proyecto

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('client');
+  protected readonly title = signal('PAYLOAD');
 }
